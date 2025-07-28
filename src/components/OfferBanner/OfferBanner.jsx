3ffd4f5567f440b1f9dfd0ab0154d924c289dd9b
SourceRn <ramnievesg@gmail.com>
2025-07-28 10:20:20 -0600

    link fixed

--- src/components/OfferBanner/OfferBanner.jsx
+++ src/components/OfferBanner/OfferBanner.jsx
@@ -4,7 +4,7 @@ import './OfferBanner.css'; // Assuming you have a CSS file for styles
 const OfferBanner = () => (
   <div className="offer-banner">
     <div className='marquee'>
-      <h3><a href="http://localhost:5173/products">🔥 Oferta de la semana: Choco Proteico a $20 y Choco Deslactosado a $30</a></h3>
+      <h3><a href="https://chocos-ecommerce.vercel.app/products">🔥 Oferta de la semana: Choco Proteico a $20 y Choco Deslactosado a $30</a></h3>
     </div>
   </div>
 );
